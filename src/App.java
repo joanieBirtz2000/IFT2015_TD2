56f518c70cdbcb920b18d7320098a4786c30fdc9
Joanie Birtz <56734772+joanieBirtz2000@users.noreply.github.com>
2023-02-12 18:10:48 -0500

    Node 3.1
    Node.java question 3.1

--- src/App.java
+++ src/App.java
@@ -1,6 +1,39 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        // Vous pouvez tester votre code ici!
-    }
+	public static void main(String[] args) throws Exception {
+		// Vous pouvez tester votre code ici!
+
+		Node premier = new Node(2);
+		premier.addValue(5);
+		premier.addValue(4);
+		System.out.println("Test addValue()");
+
+		premier.print();
+		System.out.println("Test addNode()");
+		// erreur si tu fait deuxieme new node (3,premier)
+		Node deuxieme = new Node(3);
+		deuxieme.addValue(5);
+		// deuxieme.print();
+		premier.addNode(deuxieme);
+		premier.print();
+
+		System.out.println("Test  removeLast()");
+		Node troisieme = new Node(8);
+		troisieme.addValue(4);
+		troisieme.removeLast();
+		troisieme.print();
+
+		System.out.println("Test  removeValue()");
+		premier.removeValue(5);
+		premier.print();
+
+		System.out.println("length_iteratif() et length_recurssion()");
+		Node quatrieme = new Node(10);
+		System.out.println(quatrieme.length_iteratif());
+		System.out.println(premier.length_iteratif());
+		System.out.println(quatrieme.length_recurssion());
+		System.out.println(premier.length_recurssion());
+
+		System.out.println("returnNlast()");
+		System.out.println(premier.returnNlast(1));
+	}
 }
--- src/Node.java
+++ src/Node.java
@@ -1,51 +1,108 @@
 public class Node {
-    private int value;
-    private Node next = null;
+	private int value;
+	private Node next = null;
 
-    public Node(int value){
-        // TODO
-    }
+	public Node(int value) {
+		this.value = value;
+	}
 
-    public Node(int value, Node next){
-        // TODO
-    }
+	public Node(int value, Node next) {
+		this.value = value;
+		this.next = next;
+	}
 
-    public void addValue(int value){
-        // TODO
-    }
+	public void addValue(int value) {
+		if (this.next == null) {
+			this.next = new Node(value);
+		} else {
+			this.next.addValue(value);
+		}
+	}
 
-    public void addNode(Node next){
-        // TODO
-    }
+	public void addNode(Node next) {
+		if (this.next == null) {
+			this.next = next;
+		} else {
+			this.next.addNode(next);
+		}
+	}
 
-    public void removeLast(){
-        // TODO
-    }
+	public void removeLast() {
+		// gere s'il y a un seule element
+		if (this.next == null) {
+			return;
+		}
+		if (this.next.next == null) {
+			this.next = null;
+		} else {
+			this.next.removeLast();
+		}
+	}
 
-    public void removeValue(int value){
-        // TODO
-    }
+	public void removeValue(int value) {
+		if (this.next == null) {
+			return;
+		}
+		if (this.next.value == value) {
+			this.next = this.next.next;
+			removeValue(value);
+		} else {
+			this.next.removeValue(value);
+		}
+	}
 
-    public int length_iteratif(){
-        // TODO
-        return 0;
-    }
+	public int length_iteratif() {
+		int length = 0;
+		Node current = this;
+		while (current != null) {
+			length++;
+			current = current.next;
+		}
+		return length;
+	}
 
-    public int length_recurssion(){
-        // TODO
-        return 0;
-    }
+	public int length_recurssion() {
+		if (this.next == null) {
+			return 1;
+		}
+		return 1 + this.next.length_recurssion();
+	}
 
-    public int returnNlast(int nLast){
-        // TODO
-        return 0;
-    }
+	public int returnNlast(int nLast) throws Exception {
+		Node current = this;
+		int length = length_iteratif();
 
-    public void addValue_ordered(int value){
-        // TODO
-    }
+		if (nLast == 0 || nLast > length) {
+			throw new Exception("Invalid index - fist value is " + current.value);
 
-    public void insertSort(){
-        // TODO
-    }
+		}
+
+		for (int i = 0; i < length - nLast; i++) {
+			current = current.next;
+		}
+
+		return current.value;
+	}
+
+	public void addValue_ordered(int value) {
+		// TODO
+	}
+
+	public void insertSort() {
+		// TODO
+	}
+
+	// Ma fonction print()
+	public void print() {
+		int count = 0;
+		Node current = this;
+		while (current.next != null) {
+			count++;
+			System.out.println("Node #" + count + " valeur: " + current.value);
+			current = current.next;
+
+		}
+		count++;
+		System.out.println("Node #" + count + " valeur: " + current.value);
+	}
 }
