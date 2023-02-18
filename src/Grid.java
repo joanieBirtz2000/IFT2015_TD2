ab342d079959704b6b286638ae10255b7a7d2e01
Arpad <arpadbotond.rigo@outlook.com>
2023-02-17 22:54:23 -0500

    5.1 fini

--- src/Grid.java
+++ src/Grid.java
@@ -57,16 +57,77 @@ public class Grid {
 		public void setValeur(int valeur) {
 			this.valeur = valeur;
 		}
-
-		public Cell(){
-        }
     }
     
-    // TODO
-
+    public ArrayList<Cell> grid=new ArrayList<>();
+    /**
+     * 4 columns / 3 lines
+     */
     public Grid(){
-    	Cell case1=new Cell(null,new Cell(),new Cell(),null,25);
-        List<Cell> grid= Arrays.asList(new Cell());
+    	int[] values= {25,10,1,10,1,5,5,10,1,10,1,-1};
+        for(int i=0; i<values.length;i++) {
+        	Cell cell=new Cell(null,null,null,null,values[i]);
+        	switch(i) {
+        	case 1:
+        		grid.get(0).setPointeur_droite(cell);
+        		cell.setPointeur_gauche(grid.get(0));
+        		break;
+        	case 2:
+        		grid.get(1).setPointeur_droite(cell);
+        		cell.setPointeur_gauche(grid.get(1));
+        		break;
+        	case 3:
+        		grid.get(2).setPointeur_droite(cell);
+        		cell.setPointeur_gauche(grid.get(2));
+        		break;
+        	case 4:
+        		grid.get(0).setPointeur_bas(cell);
+        		cell.setPointeur_haut(grid.get(0));
+        		break;
+        	case 5:
+        		grid.get(4).setPointeur_droite(cell);
+        		cell.setPointeur_gauche(grid.get(4));
+        		grid.get(1).setPointeur_bas(cell);
+        		cell.setPointeur_haut(grid.get(1));
+        		break;
+        	case 6:
+        		grid.get(5).setPointeur_droite(cell);
+        		cell.setPointeur_gauche(grid.get(5));
+        		grid.get(2).setPointeur_bas(cell);
+        		cell.setPointeur_haut(grid.get(2));
+        		break;
+        	case 7:
+        		grid.get(6).setPointeur_droite(cell);
+        		cell.setPointeur_gauche(grid.get(6));
+        		grid.get(3).setPointeur_bas(cell);
+        		cell.setPointeur_haut(grid.get(3));
+        		break;
+        	case 8:
+        		grid.get(4).setPointeur_bas(cell);
+        		cell.setPointeur_haut(grid.get(4));
+        		break;
+        	case 9:
+        		grid.get(8).setPointeur_droite(cell);
+        		cell.setPointeur_gauche(grid.get(8));
+        		grid.get(5).setPointeur_bas(cell);
+        		cell.setPointeur_haut(grid.get(5));
+        		break;
+        	case 10:
+        		grid.get(9).setPointeur_droite(cell);
+        		cell.setPointeur_gauche(grid.get(9));
+        		grid.get(6).setPointeur_bas(cell);
+        		cell.setPointeur_haut(grid.get(6));
+        		break;
+        	case 11:
+        		grid.get(10).setPointeur_droite(cell);
+        		cell.setPointeur_gauche(grid.get(10));
+        		grid.get(7).setPointeur_bas(cell);
+        		cell.setPointeur_haut(grid.get(7));
+        		break;
+        	}
+        	grid.add(cell);
+        }
+
     }
 
     public boolean move(Cell box){
@@ -82,4 +143,6 @@ public class Grid {
     public void solve_game(){
         // TODO
     }
+
+    
 }
